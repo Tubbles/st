08403af9240e7825fc740b7dfd625cda740d73b3
aleks <aleks.stier@icloud.com>
2019-08-11 03:15:26 +0200

    Make right-click paste
    Pressing right-click pastes from the primary-selection.
    If combined with the clipboard-patch right-click pastes from the clipboard.
    Middle-click does nothing.

--- config.def.h
+++ config.def.h
@@ -182,7 +182,7 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
+	{ XK_ANY_MOD,           Button3, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
 	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
