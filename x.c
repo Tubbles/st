da6e40f622b1978c0c93548c2d8ac2ec5984927a
Tubbles <jae91m@gmail.com>
2019-10-26 00:08:46 +0200

    apply st-clipboard-0.8.2.diff

--- x.c
+++ x.c
@@ -659,6 +659,8 @@ setsel(char *str, Time t)
 	XSetSelectionOwner(xw.dpy, XA_PRIMARY, xw.win, t);
 	if (XGetSelectionOwner(xw.dpy, XA_PRIMARY) != xw.win)
 		selclear();
+
+	clipcopy(NULL);
 }
 
 void
